d3813a10dd2c8c503f20eaaee1b30fe670c318a0
Matt Gardner <mjg82@byu.edu>
2011-12-23 23:44:15 -0500

    Debug statement while I figure out why agents don't work right

--- game/socketio_base.py
+++ game/socketio_base.py
@@ -7,6 +7,7 @@ def get_message(socketio):
         raise ConnectionLostError()
     if not (len(message) == 1 and type(message) == list):
         raise ValueError("Message wasn't a list of 1 item")
+    print 'Message received:', message[0]
     return message[0]
 
 # vim: et sw=4 sts=4
