084dd3a5b47f771941317cec595c54c2960392e8
Matt Gardner <mjg82@byu.edu>
2012-01-04 20:26:03 -0500

    Better handle heartbeat messages

--- game/socketio_base.py
+++ game/socketio_base.py
@@ -2,12 +2,18 @@
 
 
 def get_message(socketio):
-    message = socketio.recv()
-    if not socketio.connected():
-        raise ConnectionLostError()
-    if not (len(message) == 1 and type(message) == list):
-        raise ValueError("Message wasn't a list of 1 item")
-    print 'Message received:', message[0]
-    return message[0]
+    while True:
+        message = socketio.recv()
+        if not message:
+            # Probably a heartbeat
+            print 'Saw a heartbeart message'
+            continue
+        if not socketio.connected():
+            raise ConnectionLostError()
+        if not (len(message) == 1 and type(message) == list):
+            print 'Bad message:', message
+            raise ValueError("Message wasn't a list of 1 item")
+        print 'Message received:', message[0]
+        return message[0]
 
 # vim: et sw=4 sts=4
