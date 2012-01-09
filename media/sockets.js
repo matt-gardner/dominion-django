f5bd836360c40db99bb5789b78b4441e404a1d98
Matt Gardner <mjg82@byu.edu>
2012-01-09 18:09:56 -0500

    Starting to work on the web client

--- media/sockets.js
+++ media/sockets.js
@@ -1,22 +1,23 @@
-function message(obj){
-    if (!obj) {
-        // Not sure why this happens sometimes, but it does.
+function on_message(message) {
+    if (!message) {
+        // Heartbeat message; move along
         return;
     }
-    if ('count' in obj) {
-        $('#main').text(obj.count);
+    if ('connected' in message) {
+        initialize_ui(message);
+        $('#main').text(message.count);
         $('#form').css('display', 'block');
     }
-    if ('announcement' in obj) {
-        $('<em>'+esc(obj.announcement)+'</em>')
+    if ('announcement' in message) {
+        $('<em>'+esc(message.announcement)+'</em>')
         .appendTo('#main')
         .wrap('<p></p>');
     }
-    if ('available' in obj) {
+    if ('available' in message) {
         var i = 0;
-        for (i = 0; i < obj.available.length; i++) {
-            player_num = obj.available[i];
-            text = obj.available[i];
+        for (i = 0; i < message.available.length; i++) {
+            player_num = message.available[i];
+            text = message.available[i];
             $('<button onclick="player('+player_num+')">'+text+'</button>')
             .appendTo('#main')
             .wrap('<p></p>');
@@ -24,16 +25,21 @@ function message(obj){
     }
 };
 
-function send(){
-    var val = 1;
-    $.fn.socket_config.socket.send({'val': val});
+function send(message) {
+    $.fn.socket_config.socket.send(message);
 };
 
-function player(num){
-    $.fn.socket_config.socket.send({'player': num});
+function initialize_ui(message) {
 };
 
-function esc(msg){
+function refresh_ui(message) {
+};
+
+function player(num) {
+    send({'player': num});
+};
+
+function esc(msg) {
     return String(msg).replace(/</g, '&lt;').replace(/>/g, '&gt;');
 };
 
@@ -41,10 +47,11 @@ $(document).ready(function() {
     $.fn.socket_config.socket = new io.Socket(null,
             {port: $.fn.socket_config.port, rememberTransport: false});
     $.fn.socket_config.socket.connect();
-    $.fn.socket_config.socket.on('connect', function(obj) {
-      $.fn.socket_config.socket.send({'game': $.fn.game_config.game});
+    $.fn.socket_config.socket.on('connect', function(message) {
+        send({'game': $.fn.game_config.game});
     });
-    $.fn.socket_config.socket.on('message', function(obj){
-        message(obj);
+    $.fn.socket_config.socket.on('message', function(message) {
+        on_message(message);
     });
+    $.fn.player_state = function() {};
 });
